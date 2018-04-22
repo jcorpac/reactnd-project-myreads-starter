4734dd74d3cfe332270e952f458f8a343c05d482
jcorpac <jcorpac@gmail.com>
2018-04-22 16:55:42 -0500

    Updated App's management of book state No longer retrieves state from BooksAPI on every change.

--- src/App.js
+++ src/App.js
@@ -12,18 +12,36 @@ class BooksApp extends React.Component {
   }
 
   componentDidMount() {
-    this.updateBooksFromAPI();
-  }
-
-  updateBooksFromAPI() {
     BooksAPI.getAll().then((books) => {
       this.setState({books})
     });
   }
 
+  // Like findIndex, but for bookIds!
+  // Compares book id to books in this.state.books. Returns -1 if the book is not found
+  findBookIndex(book) {
+    const bookList = this.state.books;
+    for(var i = 0; i < bookList.length; i++ ) {
+      if(book.id === bookList[i].id) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
   addBookToShelf = (book, shelf) => {
-    BooksAPI.update(book, shelf)
-      .then(this.updateBooksFromAPI())
+    BooksAPI.update(book, shelf).then((state) => {
+      let bookList = this.state.books;
+      let bookIndex = this.findBookIndex(book);
+      if (bookIndex === -1) {
+        // If the book isn't in the list, then append it to the end and change bookIndex to the end of the array.
+        bookIndex = bookList.length;
+        bookList.push(book);
+      }
+      // Set the shelf for the indexed book to the proper value.
+      bookList[bookIndex].shelf = shelf;
+      this.setState({books: bookList});
+    })
   }
 
   render() {
