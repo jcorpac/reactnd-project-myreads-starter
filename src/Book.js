d471a34d5217eddafe670674536d1356bd50d62a
jcorpac <jcorpac@gmail.com>
2018-04-23 14:04:13 -0500

    Updated Book to properly display bookshelf information

--- src/Book.js
+++ src/Book.js
@@ -1,11 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function mergeAuthors(authorList) {
-  if(!authorList || authorList.length === 0) {return '';}
-  else {return authorList.join(', ')}
-}
-
 function handleThumbnail(imageLinks) {
   if(!imageLinks || !imageLinks.thumbnail) {return 'http://via.placeholder.com/128x193.png?text=No+Cover+Available'}
   else {return imageLinks.thumbnail;}
@@ -19,7 +14,7 @@ function Book ( {book, changeShelfFunc} ) {
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${handleThumbnail(book.imageLinks)})` }}></div>
           <div className="book-shelf-changer">
             <select value={book.shelf ? book.shelf : 'none'} onChange={(event) => {changeShelfFunc(book, event.target.value)}}>
-              <option value="none" disabled>Move to...</option>
+              <option value="" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
               <option value="read">Read</option>
@@ -28,7 +23,7 @@ function Book ( {book, changeShelfFunc} ) {
           </div>
         </div>
         <div className="book-title">{book.title}</div>
-        <div className="book-authors">{mergeAuthors(book.authors)}</div>
+        <div className="book-authors">{book.authors ? book.authors.join(', ') : '' }</div>
       </div>
     </li>
   )
